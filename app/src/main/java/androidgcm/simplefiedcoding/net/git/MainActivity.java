f5e4b33fc98b33d0caca8a991b8198d59a484e94
Siraj <sirajnwings@gmail.com>
2016-07-17 12:15:28 +0530

    ok

---
+++ app/src/main/java/androidgcm/simplefiedcoding/net/git/MainActivity.java
@@ -0,0 +1,15 @@
+package androidgcm.simplefiedcoding.net.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String ok,jai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
